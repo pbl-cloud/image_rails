4920331e42b6e4d5c5f009e5b0f91e3f640d9f85
shioritty <shioritty@gmail.com>
2014-05-16 19:23:28 +0900

    added created_pictures_controller, modified routes

---
+++ app/controllers/created_pictures_controller.rb
@@ -0,0 +1,14 @@
+# app/controllers/created_pictures_controller.rb
+class CreatedsPicturesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
