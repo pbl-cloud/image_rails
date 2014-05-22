7a41f38eeadb83f6f777b1ce4b7eb1e353c911d5
tuvistavie <tuvistavie@gmail.com>
2014-05-23 04:55:35 +0900

    Add basic logic to save image.

--- app/models/created_picture.rb
+++ app/models/created_picture.rb
@@ -12,11 +12,20 @@
 #
 
 class CreatedPicture < ActiveRecord::Base
+  include Hamecomic
+
   belongs_to :user
+  belongs_to :base_picture
 
   mount_uploader :uploaded_image, MixinPictureUploader
 
+  delegate :mode, to: :base_picture
+
   def process_image
+    images = make_comic(self.uploaded_image.path, self.mode)
+    images.each_with_index do |img, i|
+      img.save("img-#{i}.jpg")
+    end
   end
   handle_asynchronously :process_image
 end
