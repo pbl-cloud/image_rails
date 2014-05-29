9807fc1bd1eecf89a928d3243e189a1139e6b22d
tuvistavie <tuvistavie@gmail.com>
2014-05-30 01:32:17 +0900

    Update link files and dirs.

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,9 +12,9 @@ set :log_level, :info
 
 # set :pty, true
 
-set :linked_files, %w{config/database.yml config/settings.local.yml}
+set :linked_files, %w{config/database.yml config/settings.local.yml config/secrets.yml}
 
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/assets}
 
 set :rails_env, 'production'
 
